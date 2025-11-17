d43880073812e1d842115c91ae1771a5faaff88a
NotAShelf <raf@notashelf.dev>
2025-11-17 15:51:56 +0300

    nix: streamline source filter; move RUSTFLAGS to env attrs
    Signed-off-by: NotAShelf <raf@notashelf.dev>
    Change-Id: I9761b908d05efd7dc10d53c54ca80fb26a6a6964

--- nix/package.nix
+++ nix/package.nix
@@ -9,23 +9,29 @@
   inherit (toml) version;
 in
   rustPlatform.buildRustPackage.override {stdenv = stdenvAdapters.useMoldLinker llvm.stdenv;} {
-    RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
-
     inherit pname version;
-
-    src = builtins.path {
-      name = "${pname}-${version}";
-      path = lib.sources.cleanSource ../.;
-    };
+    src = let
+      fs = lib.fileset;
+      s = ../.;
+    in
+      fs.toSource {
+        root = s;
+        fileset = fs.unions [
+          (fs.fileFilter (file: builtins.any file.hasExt ["rs"]) (s + /src))
+          (s + /Cargo.lock)
+          (s + /Cargo.toml)
+        ];
+      };
 
     cargoLock.lockFile = ../Cargo.lock;
     enableParallelBuilding = true;
+    env.RUSTFLAGS = "-C link-arg=-fuse-ld=mold";
 
     meta = {
-      description = "A microscopic fetch script in Rust, for NixOS systems";
+      description = "Microscopic fetch script in Rust, for NixOS systems";
       homepage = "https://github.com/NotAShelf/microfetch";
       license = lib.licenses.gpl3Only;
-      maintainers = with lib.maintainers; [NotAShelf];
+      maintainers = [lib.maintainers.NotAShelf];
       mainProgram = "microfetch";
     };
   }
